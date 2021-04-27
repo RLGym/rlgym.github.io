d684897a97ea6685c425355850f754d75c280afd
Lucas Emery <lucas.emery@hotmail.com>
2021-04-28 00:51:45 +0200

    Autogenerate Docs

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -1,4 +1,81 @@
 
+const docs = [
+	{
+		id: 'introduction',
+		files: [
+			'docs/introduction/introduction.md',
+		]
+	},
+	{
+		id: 'getting-started',
+		files: [
+			'docs/getting-started/getting-started.md',
+		]
+	},
+	{
+		id: 'documentation',
+		files: [
+			'docs/documentation/placeholder.md',
+		]
+	},
+	{
+		id: 'tutorials',
+		files: [
+			'docs/tutorials/introduction_to_configuration_objects.md',
+			'docs/tutorials/reward_functions.md',
+		]
+	},
+	{
+		id: 'faq',
+		files: [
+			'docs/faq/faq.md',
+		]
+	},
+	{
+		id: 'troubleshooting',
+		files: [
+			'docs/troubleshooting/troubleshooting.md',
+		]
+	}
+]
+
+function loadFileAsync(path, container) {
+	return fetch(path)
+		.then(response => response.text())
+		.then(data => container.innerHTML = marked(data))
+}
+
+function loadFileMock(path, container) {
+	container.innerHTML = marked('## Marked in browser\n\n'+path+'\n\nRendered by **marked**.\n\n## Header 2\n\nconteeeent')
+	return Promise.resolve()
+}
+
+function generateSectionNav(article) {
+	let lastNav = document.querySelector('a[href="#'+ article.id +'"]').parentElement;
+
+	for (let header of article.getElementsByTagName('h2')) {
+		header.className = 'section-heading';
+
+		lastNav.insertAdjacentHTML('afterend',
+			'<li class="nav-item"><a class="nav-link scrollto" href="#'+ header.id +'">'+ header.innerText +'</a></li>');
+		lastNav = lastNav.nextElementSibling;
+	}
+}
+
+function loadDocsAsync() {
+	for (let article of docs) {
+		const articleElement = document.getElementById(article.id);
+		const articleContent = [];
+		article.files.forEach(file => {
+			const container = document.createElement('section');
+			container.className = 'docs-section';
+			articleElement.appendChild(container);
+			articleContent.push(loadFileAsync(file, container));
+		})
+		Promise.all(articleContent).then(() => generateSectionNav(articleElement))
+	}
+}
+
 $(window).on('load resize', function() {
    
     //Add/remove class based on browser size when load/resize
@@ -66,9 +143,9 @@ $(document).ready(function() {
     $(document).delegate('*[data-toggle="lightbox"]', 'click', function(e) {
         e.preventDefault();
         $(this).ekkoLightbox();
-    }); 
+    });
 
-    
-    
+
+	loadDocsAsync();
 
 });
