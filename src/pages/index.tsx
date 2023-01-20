0bca0c1e6cb152c4a81c3306809b305c129416a7
George Walter Colgrove IV <georgecolgrove@outlook.com>
2023-01-19 20:36:33 -0500

    Improvement

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -24,8 +24,8 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={siteConfig.title}
+      description="The Rocket League Gym">
       <HomepageHeader />
       <main>
         <HomepageFeatures />
